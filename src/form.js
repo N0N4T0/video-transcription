ddd1c2cac63641def8f1dd22e62e6026e62c05e1
Lucio <luciononato@hotmail.com>
2023-08-13 11:19:38 -0300

    fix: mock data

--- src/form.js
+++ src/form.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { startLoading, loadingMessage, stopLoading } from "./loading"
 import { getVideoId, loadVideo } from "./youtube-api"
+import { transcribeAudio } from "../server/transcribe"
 
 const form = document.querySelector('#form')
 
@@ -16,8 +17,11 @@ form.addEventListener('submit', async(e)=> {
 
     await loadVideo(url)
 
-    loadingMessage('Baixando e convertendo o vídeo')
-    await axios.get('http://localhost:3333/audio?v=' + getVideoId(url))
+    // loadingMessage('Baixando e convertendo o vídeo')
+    // await axios.get('http://localhost:3333/audio?v=' + getVideoId(url))
+
+    const data = await transcribeAudio()
+    console.log(data)
   }catch(error){
     console.log('[SUBMIT_ERROR]', error)
   }finally {
